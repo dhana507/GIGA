4cd4864b142251071c96d56fc3e956b1e42332e8
dhana507 <123790988+dhana507@users.noreply.github.com>
2024-01-19 13:52:26 +0530

    Create Coding Meetup #2 - Higher-Order Functions Series - Greet developers.py

---
+++ Coding Meetup #2 - Higher-Order Functions Series - Greet developers.py
@@ -0,0 +1,6 @@
+def greet_developers(lst): 
+    # your code here
+    pass
+    for i in lst:
+        i['greeting']='Hi '+i['firstName']+', what do you like the most about '+i['language']
+    return lst
